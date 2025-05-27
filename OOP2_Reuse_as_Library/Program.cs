3d656beb81269adc7e1a9e7457f307062b1f5149
Võ  Phú <phuvo1104@gmail.com>
2025-05-27 11:06:53 +0700

    lien ket 2 file bang cach add

---
+++ OOP2_Reuse_as_Library/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using OOP2;
+using OOP2_Reuse_as_Library;
+
+Console.OutputEncoding =Encoding.UTF8;
+FullTimeEmployees e1 = new FullTimeEmployees();
+e1.Id = 1;
+e1.Name = "teo";
+e1.IdCard = "123";
+e1.BirthDay = new DateTime(1990, 12, 25);
+Console.WriteLine("--------thong tin e1------");
+Console.WriteLine(e1);
+Console.WriteLine("==> AGE: " + e1.TinhTuoi());

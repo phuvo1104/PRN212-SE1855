136a8770afb90b45385e6affffb1afebbdf83eaa
Võ  Phú <phuvo1104@gmail.com>
2025-06-13 10:23:17 +0700

    linq

--- Lucy_SaleManagement/Program.cs
+++ Lucy_SaleManagement/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Lucy_SaleManagement;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
-string connectionString = @"server=localhost;database=MyStore;uid=sa;pwd=12345";
+string connectionString = @"server=localhost;database=Lucy_SalesData;uid=sa;pwd=12345";
 Lucy_SalesDataDataContext context = new Lucy_SalesDataDataContext(connectionString);
 //cau1 lay chi tiet thong tin khach hangn khi biet ma so 
 int mkh = 1;
@@ -23,17 +23,3 @@ if(cust != null)
 }
 //cau 3 nagn cap cau 23
 //bo sung them mot cot hien thi gia tri cua don
-if (cust != null)
-{
-    Console.WriteLine("Danh sách hóa đơn của khách hàng:");
-
-    Console.WriteLine("Mã đơn\tNgày đặt\t\tTổng giá trị (VNĐ)");
-
-    foreach (Order od in cust.Orders)
-    {
-        // Tính tổng giá trị đơn hàng
-        decimal total = od.OrderDetails.Sum(d => d.UnitPrice * d.Quantity * (1 - (decimal)d.Discount));
-
-        Console.WriteLine($"{od.OrderID}\t{od.OrderDate:dd/MM/yyyy}\t{total:N0}");
-    }
-}
